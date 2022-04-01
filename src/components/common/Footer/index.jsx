ac21acaaf18c202b4a52348ff6135fff56bd8cdc
Minh Trịnh Ngô Tân <tanminh.tn30102001@gmail.com>
2022-04-01 13:41:57 +0700

    singleshop

---
+++ src/components/common/Footer/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Logo from '../Logo';
+import './styles.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        We're a team of Professional cooks who are excited about their food,
+        amazing skills and expertiness in cooking.
+        
+      </p>
+      <Logo />
+    </footer>
+  );
+};
+
+export default Footer;
